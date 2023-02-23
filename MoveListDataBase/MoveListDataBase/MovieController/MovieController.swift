6e9aa6f70eae5e5e67a5d356d59cfafb8f0ed4d8
Matthew Hill <matthill265@gmail.com>
2023-02-23 09:53:17 -0700

    made the Movie Model

---
+++ MoveListDataBase/MoveListDataBase/MovieController/MovieController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieController.swift
+//  MoveListDataBase
+//
+//  Created by Matthew Hill on 2/23/23.
+//
+
+import Foundation
+
+class MovieController {
+    
+    static func fetchMovieWith(searchTerm: String, completion: @escaping ([Movie]?) -> Void {
+        
+    }
+}
