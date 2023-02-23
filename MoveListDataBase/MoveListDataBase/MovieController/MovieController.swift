357b04eb2a7e05fa29563774370360cd98a2a57b
Matthew Hill <matthill265@gmail.com>
2023-02-23 10:09:22 -0700

    Have my final url on my fetch movie functions

--- MoveListDataBase/MoveListDataBase/MovieController/MovieController.swift
+++ MoveListDataBase/MoveListDataBase/MovieController/MovieController.swift
@@ -9,7 +9,16 @@ import Foundation
 
 class MovieController {
     
-    static func fetchMovieWith(searchTerm: String, completion: @escaping ([Movie]?) -> Void {
+    static func fetchMovieWith(searchTerm: String, completion: @escaping ([Movie]?) -> Void) {
         
+        // construct URL
+        guard let baseURL = URL(string: Constants.MovieURL.movieBaseURL) else { completion(nil) ; return }
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let apKeyQuery = URLQueryItem(name: Constants.MovieURL.apiKeyKey, value: Constants.MovieURL.apiKeyValue)
+        let searchQuery = URLQueryItem(name: Constants.MovieURL.movieQueryKey, value: searchTerm)
+        let filterQuery = URLQueryItem(name: Constants.MovieURL.adultQueryKey, value: Constants.MovieURL.adultFilterValue)
+        
+        guard let finalURL = urlComponents?.url else {completion(nil) ; return}
+        print(finalURL)
     }
 }
